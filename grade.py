85429946b717721011326b2341cb098bfaca0994
marleypyp <bacanarush@gmail.com>
2025-04-11 18:09:32 -0300

    Update grade.py

--- grade.py
+++ grade.py
@@ -5,5 +5,9 @@ elif score >= 80:
         print ("grade: B")
 elif score >= 70:
         print ("grade: C")
+elif score >= 60:
+        print ("grade: D")
+elif score >= 50:
+        print("grade: E")
 else:
         print("F")
